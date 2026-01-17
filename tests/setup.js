b6879e6390cdc29d15020258c04339081c61f3e5
ILikeMyChickenBurnt <81396423+ILikeMyChickenBurnt@users.noreply.github.com>
2026-01-16 22:15:20 -0600

    Remove global test utilities from Jest setup

--- tests/setup.js
+++ tests/setup.js
@@ -9,12 +9,3 @@ jest.mock('electron', () => require('./__mocks__/electron'));
 
 // Suppress console.log in tests (optional - comment out for debugging)
 // global.console.log = jest.fn();
-
-// Global test utilities
-global.testHelpers = {
-  // Helper to create a test database instance
-  createTestDb: () => {
-    const Database = require('better-sqlite3');
-    return new Database(':memory:');
-  }
-};
